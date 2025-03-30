71167c16ce038d561a6bf5aecb5443748b112559
MultipleCoffee <67815516+MultipleCoffee@users.noreply.github.com>
2025-03-31 01:39:06 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
-package main 
+package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	title := os.Getenv("APP_TITLE")
+	if title == "" {
+		title = "Default Go Web App"
+	}
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
 		data := map[string]string{
-			"Title": "My Simple Go Web App",
+			"Title": title,
 		}
 		tmpl.Execute(w, data)
 	})
 
-	http.ListenAndServe(":8080", nil)
+	log.Printf("Server starting on port %s...", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
